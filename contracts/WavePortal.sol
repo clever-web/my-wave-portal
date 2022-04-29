a3b62588f391ee95c09a1ec0878950a9e1d43d08
superstar <83311833+clever-web@users.noreply.github.com>
2022-04-29 16:16:24 +0300

    section1-commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    
+    uint256 totalWaves;
+
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,37 @@
+
+const main = async () => {
+
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+  
+    waveCount = await waveContract.getTotalWaves();
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0); // exit Node process without error
+    } catch (error) {
+      console.log(error);
+      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+    }
+    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+  };
+  
+  runMain();
