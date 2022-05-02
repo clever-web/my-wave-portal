443f655322f58382a9f0e164c5b7ad51dab251ee
superstar <83311833+clever-web@users.noreply.github.com>
2022-05-02 18:12:54 +0300

    complete-commit

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -39,10 +39,7 @@ contract WavePortal {
 
     function wave(string memory _message) public {
 
-        require(
-            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
-            "Wait 15m"
-        );
+        require(lastWavedAt[msg.sender] + 30 seconds < block.timestamp, "Must wait 30 seconds before waving again.");
 
         /*
          * Update the current timestamp we have for the user
