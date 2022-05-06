4f2a859bee2699406eb34050e4fc37986ae3fee7
superstar <83311833+clever-web@users.noreply.github.com>
2022-05-06 03:28:15 +0300

    update-run.js

--- scripts/run.js
+++ scripts/run.js
@@ -27,8 +27,8 @@
       const waveTxn = await waveContract.wave("This is wave #1");
       await waveTxn.wait();
     
-      const waveTxn2 = await waveContract.wave("This is wave #2");
-      await waveTxn2.wait();
+      // const waveTxn2 = await waveContract.wave("This is wave #2");
+      // await waveTxn2.wait();
 
       // const [_, randomPerson] = await hre.ethers.getSigners();
       // waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
